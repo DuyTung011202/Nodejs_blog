f6103efb6c4036e19c6aa5006b6700f07684c8b8
Duy Tùng <86777394+DuyTung011202@users.noreply.github.com>
2024-05-10 14:52:39 +0700

    Install Prettier Lint-Staged Husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,12 +1,12 @@
 class NewsController {
-  // [GET] /news
-  index(req, res) {
-    res.render("news");
-  }
-  // [GET] /news/:slugs
-  show(req, res) {
-    res.send("News Details");
-  }
+    // [GET] /news
+    index(req, res) {
+        res.render('news');
+    }
+    // [GET] /news/:slugs
+    show(req, res) {
+        res.send('News Details');
+    }
 }
 
 module.exports = new NewsController();
